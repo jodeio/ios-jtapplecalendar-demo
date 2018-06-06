db05d324f6c12fbc04f401052f59d83302a09c9a
dev-joshua <joshua.deguzman@xurpas.com>
2018-06-06 15:35:17 +0800

    Add/Setup calendar

---
+++ ios-jtapplecalendar-demo/Cells/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  ios-jtapplecalendar-demo
+//
+//  Created by Joshua de Guzman on 06/06/2018.
+//  Copyright © 2018 xrojan. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
--- ios-jtapplecalendar-demo/ViewController.swift
+++ ios-jtapplecalendar-demo/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
-
+    // MARK: - Outlet declaration
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    // MARK: - Variable declaration
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+// MARK: Additional protocol classes
+// JTAppleCalendar Protocols
+// delegate and data source is set in the storyboard builder
+
+// JTAppleCalendarViewDataSource Protocol
+extension ViewController: JTAppleCalendarViewDataSource{
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2000 01 01")!
+        let endDate = formatter.date(from: "2080 01 01")!
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 5,
+                                                 calendar: Calendar.current,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfRow,
+                                                 firstDayOfWeek: .sunday,
+                                                 hasStrictBoundaries: true)
+        
+        
+        return parameters
+    }
+}
+
+// JTAppleCalendarViewDelegate Protocol
+extension ViewController: JTAppleCalendarViewDelegate{
+    // MARK
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        //
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+}
